4284f4d5e9bb5685e74df76a1ff1faac36bd0ad4
satoshi-r <rgeargerge@yandex.ru>
2019-10-06 19:22:08 +0300

    added back-top

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
-  
+  $(function () {
+    $(".back-top").hide();
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 600) {
+        $(".back-top").fadeIn();
+      } else {
+        $(".back-top").fadeOut();
+      }
+    });
+
+    $(".back-top").click(function () {
+      $("body,html").animate({
+        scrollTop: 0
+      }, 800);
+      return false;
+    });
+  });
 });
